932a04fadc24ec70ff805f0d7b46c9b988fefe06
Noi <noi.patsin@gmail.com>
2019-07-31 17:43:07 -0700

    edit challenge4

--- independent_practices/ip_week_4/js_3-6_practice/js/challenge4.js
+++ independent_practices/ip_week_4/js_3-6_practice/js/challenge4.js
@@ -2,26 +2,29 @@ function fizzbuzz(number) {
 
     if (isNaN(number)) { 
         // console.log("The input need to be number"); 
-        return ("The input needs to be number");
+        // return ("The input needs to be number");
       }else if (Math.sign(number) === -1) {
         console.log("Warning! Please in put positive number")
       } else {
           
-        for (let i = 0; i < number; i++) {
-            if (number % 3 === 0 && number % 5 === 0) {
-                return("FizzBuzz");
-            } else if (number % 3 === 0) {
-                return("Fizz");
-            } else if (number % 5 === 0) {
-                return("Buzz");
+        for (let i = 0; i <= number; i++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log("FizzBuzz");
+                // return("FizzBuzz");
+            } else if (i % 3 === 0) {
+                console.log("Fizz");
+                // return("Fizz");
+            } else if (i % 5 === 0) {
+                console.log("Buzz");
+                // return("Buzz");
             } else {
-                console.log("No Fizz or Buzz")
+                console.log(i)
             }
       }
     }
     return;
 }
-console.log(fizzbuzz("x"));
-console.log(fizzbuzz(3));
-console.log(fizzbuzz(10));
+// console.log(fizzbuzz("x"));
+// console.log(fizzbuzz(3));
+// console.log(fizzbuzz(10));
 console.log(fizzbuzz(15));
