ee24c2b55d8af1a9395d4eebca9cb78abfd31d2b
Noi <noi.patsin@gmail.com>
2019-07-31 17:59:27 -0700

    update fizzBuzz answer

--- independent_practices/ip_week_4/js_3-6_practice/js/challenge4.js
+++ independent_practices/ip_week_4/js_3-6_practice/js/challenge4.js
@@ -1,30 +1,38 @@
 function fizzbuzz(number) {
+    // if (number === 0) {
+    //     console.log("");
+    // }
 
-    if (isNaN(number)) { 
-        // console.log("The input need to be number"); 
-        // return ("The input needs to be number");
-      }else if (Math.sign(number) === -1) {
-        console.log("Warning! Please in put positive number")
-      } else {
-          
-        for (let i = 0; i <= number; i++) {
-            if (i % 3 === 0 && i % 5 === 0) {
-                console.log("FizzBuzz");
-                // return("FizzBuzz");
-            } else if (i % 3 === 0) {
-                console.log("Fizz");
-                // return("Fizz");
-            } else if (i % 5 === 0) {
-                console.log("Buzz");
-                // return("Buzz");
-            } else {
-                console.log(i)
-            }
-      }
+    // if (number !== number) {
+    //     return ("not a number")
+    // }
+
+    if (!number && isNaN(number)) {  
+        console.log("it is not the number"); 
+        return ("NaN");
     }
-    return;
+    if (number < 0) {
+        console.log("Warning! this is not a positive number")
+    } 
+        
+    for (let i = 1; i <= number; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+            // return("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+            // return("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+            // return("Buzz");
+        } else {
+            console.log(i)
+        }
+    }
+    
+    // return 'Noi';
 }
 // console.log(fizzbuzz("x"));
-// console.log(fizzbuzz(3));
-// console.log(fizzbuzz(10));
-console.log(fizzbuzz(15));
+console.log(fizzbuzz(3));
+// fizzbuzz(10);
+// console.log(fizzbuzz(0));
