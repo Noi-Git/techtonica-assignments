662e4481fd7006008b4fc9ca30718c7dffa8a788
Noi <noi.patsin@gmail.com>
2019-07-31 17:16:47 -0700

    update challenge3 practice/review

---
+++ independent_practices/ip_week_4/js_3-6_practice/js/challenge3.js
@@ -0,0 +1,11 @@
+function calculateSum(number) {
+    // sum = number * (number+1);
+    // console.log(sum);
+    // return sum;
+    let sum = 0;
+    for (let i = 0; i <= number; i++)
+        sum += i
+        console.log(sum)
+}
+    // return sum;
+calculateSum(10)
