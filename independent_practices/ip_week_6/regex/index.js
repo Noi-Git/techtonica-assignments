45d4c50752d7b59654ce2d1d5d55574420d9727c
Noi <noi.patsin@gmail.com>
2019-08-15 20:39:06 -0700

    finish regex independent practice

--- independent_practices/ip_week_6/regex/index.js
+++ independent_practices/ip_week_6/regex/index.js
@@ -1,5 +1,4 @@
 // Write a function that takes a word and returns true if the word ends with tonica
-/*
 function regEx(word) {
     let regex = /tonica/
     // console.log(regex.test(word));
@@ -9,30 +8,25 @@ function regEx(word) {
     }
 }
 regEx("techtonica")
-*/
+
  
 // Write a function expression that takes a string replaces all instances of symantec with semantic?
-/*
 let replaceWord = function(sentence) {
     let originalWord = "symantec";
     return sentence.replace(originalWord, "semantic");
 }
 console.log(replaceWord("this is toppic about symantec. symantec is the name of a company"))
-*/
+
 
 // Write a function that takes a string and deletes all words that end in ing?
-
-/*
 let deleteIng = function(word) {
     let afterDelete = word.replace(/\w+(ing)/g, "");
-    console.log(afterDelete)
     return afterDelete;
 }
 console.log(deleteIng("eatting and sleeping while sitting at the desk."))
-*/
+
 
 //Write a function that takes a string and returns true if it is an email address? Compare your answer with your neighbor.
-/*
 function emailAddress(email) {
 
     if (email.match(/\w+(@)/)) {
@@ -42,9 +36,12 @@ function emailAddress(email) {
     }
 
 }
-console.log(emailAddress("eattinggmail.com"))
-*/
+console.log(emailAddress("eatting@gmail.com"))
+
 
 //Write a function that takes a string containing names separated by commas like "Leah, Russell, Michelle" and returns an array of names, ["Leah", "Russell", "Michelle"]
-
-
+function newName(name) {
+let nameArray = name.match(/.\w+./g)
+return nameArray;
+}
+console.log(newName("Leah, Russell, Michelle"))
